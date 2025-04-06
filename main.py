1cfd216d5aac3d7433c5083c7fa08aa8ff78b09b
rol506 <rolromva@gmail.com>
2025-04-06 19:44:00 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
+
+from SETTINGS import telegram_token
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=telegram_token)
+
+dp = Dispatcher()
+
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    await message.answer("Hello from RUIN KEEPERS EPTA")
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
