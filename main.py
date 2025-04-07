677ca36ad47368b7450893487ea8e82764202971
fluttershY_ <yurchuk.pasha10@mail.ru>
2025-04-07 12:52:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,6 +13,14 @@ dp = Dispatcher()
 
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
+    if True: # если чел зареган
+        pass
+    else:
+        message_text = ('Здравствуйте! Это бот независимой организации "Хранители Руин". Через него менеджеры могут '
+                        'взаимодействовать с записями пользователей и добавлять новые события. Введите ваш одноразовый '
+                        'персональный код, чтобы получить доступ.')
+
+        await bot.send_message(message.chat.id, message_text)
     await message.answer("Hello from RUIN KEEPERS EPTA")
 
 async def main():
