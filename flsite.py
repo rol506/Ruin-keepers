9fb3fd1db21b2de324430256f27d23cc33f6f30f
Лев <kirillov.lev@outlook.com>
2025-04-07 19:09:32 +0200

    1

--- flsite.py
+++ flsite.py
@@ -65,5 +65,6 @@ def events():
 def error_404(error): 
     return render_template("error_404.html", menu=menu)
 
+
 if __name__ == "__main__":
     app.run("0.0.0.0", port=4221, debug=app.config.get("DEBUG"))
