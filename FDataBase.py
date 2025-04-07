ef80fd85031bcab658d7665ecf9d5bb1ce6ca7eb
Лев <kirillov.lev@outlook.com>
2025-04-07 16:25:26 +0200

    1

--- FDataBase.py
+++ FDataBase.py
@@ -11,10 +11,11 @@ class FDataBase:
         "Who", "Phone", "Source", "Total", "Age", "Children", "XP", "New", "Prepayment", "Destination", "Comment", "Lunch", "Reminder", "Confirmation", "Balance", "Destination", "Nickname", "Feedback"
 
         ]
-        col_letter = chr(65 + i)  # Преобразуем 0 -> 'A', 1 -> 'B', ...
-        cell = f"{col_letter}1"
-        self.update_value(cell, headers[i])
-        self.format(cell, {"textFormat": {"bold": True}})
+        for i in range(len(headers)):
+            col_letter = chr(65 + i)  # Преобразуем 0 -> 'A', 1 -> 'B', ...
+            cell = f"{col_letter}1"
+            self.worksht.update_value(cell, headers[i])
+            self.worksht.format(cell, {"textFormat": {"bold": True}})
 if __name__ == "__main__":
     db = FDataBase("ruin-keepers.json")
     db.createDB()
