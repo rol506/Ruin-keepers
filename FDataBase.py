cb4d3049c4b215eac1e5d0727d0ad01b89538729
Лев <kirillov.lev@outlook.com>
2025-04-07 14:55:19 +0200

    1

--- FDataBase.py
+++ FDataBase.py
@@ -1,4 +1,18 @@
+import pygsheets
+import numpy as np
 
-class FDataBase:
-    def __init__(self):
-        pass
+gc = pygsheets.authorize()
+
+# Open spreadsheet and then worksheet
+sh = gc.open('my new sheet')
+wks = sh.sheet1
+
+# Update a cell with value (just to let him know values is updated ;) )
+wks.update_value('A1', "Hey yank this numpy array")
+my_nparray = np.random.randint(10, size=(3, 4))
+
+# update the sheet with array
+wks.update_values('A2', my_nparray.tolist())
+
+# share the sheet with your friend
+sh.share("myFriend@gmail.com")
