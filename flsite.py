9ebd89832371ddaeba2a638a36f655b8a2b59bb6
rol506 <rolromva@gmail.com>
2025-04-07 17:08:11 +0200

    Ladno

--- flsite.py
+++ flsite.py
@@ -1,10 +1,13 @@
-from flask import Flask, render_template, redirect, g
-
+from flask import Flask, render_template
+from flask_login import LoginManager
 from SETTINGS import web_app_secret_key, web_app_debug
 
 app = Flask("ruinKeeper")
 app.config.update(SECRET_KEY=web_app_secret_key)
 app.config.update(DEBUG=web_app_debug)
+
+loginManager = LoginManager()
+loginManager.init_app(app)
 
 menu = [
         {
@@ -24,6 +27,27 @@ menu = [
         #    "url": "/create"
         #} 
 ]
+
+class User:
+    def __init__(self, userID: str):
+        #load user from db
+        self.userID = userID
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self) -> str:
+        return self.userID;
+
+@loginManager.user_loader
+def loadUser(user_id):
+    return User(user_id)
 
 @app.route("/")
 @app.route("/home")
