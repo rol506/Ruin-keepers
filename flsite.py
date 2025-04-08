33629d79a5187aae61ff5c02092d85dc5eb2929c
rol506 <rolromva@gmail.com>
2025-04-08 10:18:15 +0200

    fix

--- flsite.py
+++ flsite.py
@@ -123,22 +123,9 @@ def walks():
 def gallery():
     return render_template("gallery.html", menu=menu)
 
-
-
-
-
-
-
-
-
-
-
-
-
 @app.errorhandler(404)
 def error_404(error): 
     return render_template("error_404.html", menu=menu)
-
 
 if __name__ == "__main__":
     app.run("0.0.0.0", port=4221, debug=app.config.get("DEBUG"))
